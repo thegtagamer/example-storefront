02178eeff18caa34b625a533fc764d9b02c97ee8
Mike Murray <hellomikemurray@gmail.com>
2018-10-23 11:15:38 -0700

    feat: add config variable for redirect feature flag

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   dev: process.env.NODE_ENV !== "production",
   appPath: process.env.NODE_ENV === "production" ? "./build/app" : "./src",
-  enableSPARouting: process.env.ENABLE_SPA_ROUTING === "true"
+  enableSPARouting: process.env.ENABLE_SPA_ROUTING === "true",
+  enableRedirects: process.env.ENABLE_REDIRECTS === "true"
 };
 
